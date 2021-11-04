420da3950fbf2815786c4760e50be0b41da663ae
Liudmila Melezhikova <melezhikova@yandex.ru>
2021-11-04 22:02:49 +0500

    forth commit

--- src/js/e2e/e2e.server.js
+++ src/js/e2e/e2e.server.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const config = require('../../webpack.config');
+const config = require('../../../webpack.config');
 
 const server = new WebpackDevServer(webpack(config), {});
 server.listen(9000, 'localhost', (err) => {
